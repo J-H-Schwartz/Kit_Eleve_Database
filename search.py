58460119441c27ff600476073c4b7e4d54f76b51
Jonathan <schwartz.jo@laposte.net>
2020-01-31 13:24:50 +0100

    Modification de la fonction de tri de recherche, avec double tri par nom et prix

--- search.py
+++ search.py
@@ -6,10 +6,8 @@ def search_bdd(mycursor, mydb):
         if choix == 'N':
             search = input("Entrez-votre recherche : ")
             search = '\'' + search + '\''
-            mycursor.execute("SELECT * FROM plante WHERE INSTR(upper(nom), {})ORDER BY nom LIMIT 3".format(search.upper()))
+            mycursor.execute("SELECT * FROM plante WHERE INSTR(upper(nom), {})ORDER BY nom ASC, prix DESC".format(search.upper()))
             myresult = mycursor.fetchall()
-            mycursor2.execute("SELECT * FROM plante WHERE INSTR(upper(nom), {})ORDER BY prix LIMIT 3,100".format(search.upper()))
-            myresult += mycursor2.fetchall()
             break
         elif choix == 'I':
             search = input("Entrez tout ou partie de l'id de la plante recherchée : ")
